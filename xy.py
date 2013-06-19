8375aea76d0145264388532edae0e6091ea958d8
hgwca <hgwca.me@gmail.com>
2013-06-19 15:55:13 +0200

    Add the intercept and the final result

--- xy.py
+++ xy.py
@@ -1,24 +1,19 @@
+from __future__ import division
 #collecting values
-Ya= raw_input("Ya = ")
-Yb = raw_input("Yb = ")
-Xa = raw_input("Xa = ")
-Xb = raw_input("Yb = ")
+Xa = float(raw_input("Xa = "))
+Ya= float(raw_input("Ya = "))
+Xb = float(raw_input("Xb = "))
+Yb = float(raw_input("Yb = "))
 #cacul Y
-caculY = ( int(Yb) - int(Ya) )
+caculY = float(Yb) - float(Ya)
 #Cacul X
-caculX =  ( int(Xb) - int(Xa) )
+caculX = float(Xb) - float(Xa)
 #Show result
+m= float(caculY) / float(caculX)
 print "The result as a fraction is: %s / %s" %(caculY,caculX)
-#request if we want to Integer division
-DivEntier = raw_input ("do you want the result to an integer division ? Yes/No : ")
-print str(DivEntier)
-#into tiny
-DivEntier_mini= DivEntier.lower()
+print "The result as a number is:  %s" %(m)
 
-#cacul result in integer division
-if "yes" in  DivEntier_mini:
-        cacul = ( int(Yb) - int(Ya) ) / ( int(Xb) - int(Xa) )
-
-        print "The result is : %s" %(cacul)
-else:
-        print "It's OK"
+#calculation of the intercept 
+p = float(Yb) - (float(m)*float(Xb))
+print "the intercept is : %s" %(p)
+print "the equation of the line is y= %sx + %s" %(m,p)
